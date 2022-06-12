637002fd5d0e5ce7fddeb6ca1aa1f57f468ee36d
Avinash Reddy Arutla <codekurama@gmail.com>
2022-06-12 14:57:46 +0530

    Completed the basic chat functionality by using SocketIO emissions and catchings

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 
 app = Flask(__name__)
 socketio = SocketIO(app)  # Creating a SocketIO instance for the flask app
@@ -20,9 +20,18 @@ def chat():
     else:
         return redirect(url_for('home'))  # we can directly use redirect('/'), but url_for is more organised
 
+
 @socketio.on('join_room')
 def handle_join_room_event(data):
-    app.logger.info(f"{data['username']} has joined the room {data['room']}")
+    app.logger.info(f"{data['username']} has joined the room {data['room_id']}")
+    join_room(data['room_id'])
+    socketio.emit('join_room_announcement', data)
+
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info(f"{data['username']} has sent {data['message']} to the room {data['room_id']}")
+    socketio.emit('receive_message', data, room=data['room_id'])
 
 if __name__ == '__main__':
     socketio.run(app,debug=True)
