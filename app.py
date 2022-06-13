39d73bcf149beba34b60df54c3993a611be1153f
Avinash Reddy Arutla <codekurama@gmail.com>
2022-06-13 15:39:25 +0530

    added signup part

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_socketio import SocketIO, join_room
+from pymongo.errors import DuplicateKeyError
 
-from db import get_user
+from db import get_user, save_user
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login' # for the login required decorator, redirects to the route where to login
 
 socketio = SocketIO(app)  # Creating a SocketIO instance for the flask app
 
@@ -20,6 +21,7 @@ def home():
 
 
 @app.route('/chat')
+@login_required
 def chat():
     username = request.args.get('username')  # used to fetch the data from the REST request
     room_id = request.args.get('room_id')
@@ -74,6 +76,26 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            save_user(username,email,password)
+        except DuplicateKeyError:
+            message = "User Exists"
+
+        return redirect(url_for('home'))
+
+    return render_template('signup.html', message=message)
 
 
 if __name__ == '__main__':
